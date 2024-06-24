87fda64efe3e2748d72e97dfdc564fc4c68ae64e
Jesse Ezell <jesse.ezell@timu.com>
2024-06-24 09:37:21 -0700

    better camera support for web

--- lib/src/web/rtc_video_view_impl.dart
+++ lib/src/web/rtc_video_view_impl.dart
@@ -1,9 +1,13 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'dart:js_interop';
+import 'dart:js_util';
 
 import 'package:dart_webrtc/dart_webrtc.dart';
 import 'package:web/web.dart' as web;
+import 'dart:ui' as ui;
+import 'dart:ui_web' as ui_web;
 import 'package:webrtc_interface/webrtc_interface.dart';
 
 import 'rtc_video_renderer_impl.dart';
@@ -16,8 +20,10 @@ class RTCVideoView extends StatefulWidget {
     this.mirror = false,
     this.filterQuality = FilterQuality.low,
     this.placeholderBuilder,
+    this.useHtmlElementView = false,
   }) : super(key: key);
 
+  final bool useHtmlElementView;
   final RTCVideoRenderer _renderer;
   final RTCVideoViewObjectFit objectFit;
   final bool mirror;
@@ -42,16 +48,82 @@ class RTCVideoViewState extends State<RTCVideoView> {
         widget.objectFit == RTCVideoViewObjectFit.RTCVideoViewObjectFitContain
             ? 'contain'
             : 'cover';
+    frameCallback(0.toJS, 0.toJS);
   }
 
   void _onRendererListener() {
     if (mounted) setState(() {});
   }
 
+  int? callbackID;
+
+  void getFrame(web.HTMLVideoElement element) {
+    callbackID =
+        element.requestVideoFrameCallbackWithFallback(frameCallback.toJS);
+  }
+
+  void cancelFrame(web.HTMLVideoElement element) {
+    if (callbackID != null) {
+      element.cancelVideoFrameCallbackWithFallback(callbackID!);
+    }
+  }
+
+  void frameCallback(JSAny now, JSAny metadata) {
+    final web.HTMLVideoElement? element = videoRenderer.videoElement;
+    if (element != null) {
+      // only capture frames if video is playing (optimization for RAF)
+      if (element.readyState > 2) {
+        capture().then((_) async {
+          getFrame(element);
+        });
+      } else {
+        getFrame(element);
+      }
+    } else {
+      if (mounted) {
+        Future.delayed(Duration(milliseconds: 100)).then((_) {
+          frameCallback(0.toJS, 0.toJS);
+        });
+      }
+    }
+  }
+
+  ui.Image? capturedFrame;
+  num? lastFrameTime;
+  Future<void> capture() async {
+    final element = videoRenderer.videoElement!;
+    if (lastFrameTime != element.currentTime) {
+      lastFrameTime = element.currentTime;
+      try {
+        final ui.Image img = await ui_web.createImageFromTextureSource(element,
+            width: element.videoWidth,
+            height: element.videoHeight,
+            transferOwnership: false);
+
+        if (mounted) {
+          setState(() {
+            capturedFrame?.dispose();
+            capturedFrame = img;
+          });
+        }
+      } on web.DOMException catch (err) {
+        lastFrameTime = null;
+        if (err.name == 'InvalidStateError') {
+          // We don't have enough data yet, continue on
+        } else {
+          rethrow;
+        }
+      }
+    }
+  }
+
   @override
   void dispose() {
     if (mounted) {
       super.dispose();
+    }
+    if (videoRenderer.videoElement != null) {
+      cancelFrame(videoRenderer.videoElement!);
     }
   }
 
@@ -67,14 +139,41 @@ class RTCVideoViewState extends State<RTCVideoView> {
   }
 
   Widget buildVideoElementView() {
-    return HtmlElementView.fromTagName(
-        tagName: "div",
-        onElementCreated: (element) {
-          final div = element as web.HTMLDivElement;
-          div.style.width = '100%';
-          div.style.height = '100%';
-          div.appendChild(videoRenderer.videoElement!);
-        });
+    if (widget.useHtmlElementView) {
+      return HtmlElementView.fromTagName(
+          tagName: "div",
+          onElementCreated: (element) {
+            final div = element as web.HTMLDivElement;
+            div.style.width = '100%';
+            div.style.height = '100%';
+            div.appendChild(videoRenderer.videoElement!);
+          });
+    } else {
+      return Stack(children: [
+        Positioned.fill(
+            child: HtmlElementView.fromTagName(
+                tagName: "div",
+                isVisible: false,
+                onElementCreated: (element) {
+                  final div = element as web.HTMLDivElement;
+                  div.style.width = '100%';
+                  div.style.height = '100%';
+                  div.style.pointerEvents = 'none';
+                  div.style.opacity = '0';
+                  div.appendChild(videoRenderer.videoElement!);
+                })),
+        if (capturedFrame != null)
+          Positioned.fill(
+              child: RawImage(
+                  image: capturedFrame,
+                  fit: switch (widget.objectFit) {
+                    RTCVideoViewObjectFit.RTCVideoViewObjectFitContain =>
+                      BoxFit.contain,
+                    RTCVideoViewObjectFit.RTCVideoViewObjectFitCover =>
+                      BoxFit.cover,
+                  })),
+      ]);
+    }
   }
 
   @override
@@ -93,4 +192,30 @@ class RTCVideoViewState extends State<RTCVideoView> {
       },
     );
   }
+}
+
+typedef _VideoFrameRequestCallback = JSFunction;
+
+extension _HTMLVideoElementRequestAnimationFrame on web.HTMLVideoElement {
+  int requestVideoFrameCallbackWithFallback(
+      _VideoFrameRequestCallback callback) {
+    if (hasProperty(this, 'requestVideoFrameCallback')) {
+      return requestVideoFrameCallback(callback);
+    } else {
+      return web.window.requestAnimationFrame((double num) {
+        callback.callAsFunction(this, 0.toJS, 0.toJS);
+      }.toJS);
+    }
+  }
+
+  void cancelVideoFrameCallbackWithFallback(int callbackID) {
+    if (hasProperty(this, 'requestVideoFrameCallback')) {
+      cancelVideoFrameCallback(callbackID);
+    } else {
+      web.window.cancelAnimationFrame(callbackID);
+    }
+  }
+
+  external int requestVideoFrameCallback(_VideoFrameRequestCallback callback);
+  external void cancelVideoFrameCallback(int callbackID);
 }
